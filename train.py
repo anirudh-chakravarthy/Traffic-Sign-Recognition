d1f4d27c0592b4cecd254dfa9271c0eb2c4d6f7d
Anirudh Chakravarthy <anirudh.s.chakravarthy@gmail.com>
2019-06-16 19:53:55 +0530

    Supporting all models

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import os 
+import sys
 import argparse
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -17,8 +18,28 @@ np.random.seed(seed)
 
 def train(args):
 	base_dir = args.base_dir
+
+	if args.model == 'LeNet_baseline':
+		input_shape = (32, 32, 3)
+		model = LeNet_baseline(input_shape=input_shape, num_classes=43)
+	elif args.model == 'LeNet_modified':
+		input_shape = (32, 32, 3)
+		model = LeNet_modified(input_shape=input_shape, num_classes=43)
+	elif args.model == 'AlexNet':
+		input_shape = (227, 227, 3)
+		model = AlexNet(input_shape=input_shape, num_classes=43)
+	elif args.model == 'VGG16':
+		input_shape = (224, 224, 3)
+		model = VGG16(input_shape=input_shape, num_classes=43)
+	elif args.model == 'VGG19':
+		input_shape = (224, 224, 3)
+		model = VGG19(input_shape=input_shape, num_classes=43)
+	else:
+		print('Please choose an implemented model!')
+		sys.exit()
+
 	# get training dataset
-	x, y = input_preprocess(base_dir, (32,32,3))
+	x, y = input_preprocess(base_dir, input_shape=input_shape)
 	# split dataset into train and val set
 	x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=seed)
 	print("Train images loaded")
@@ -27,14 +48,12 @@ def train(args):
 	y_train = one_hot(y_train)
 	y_val = one_hot(y_val)
 
-	model = LeNet_modified(input_shape=(32,32,3), num_classes=43)
+	# train the model
+	callbacks = get_callbacks(args.checkpoint, args.model)
 	model.compile(optimizer=Adam(lr=1e-3),
 				  loss='categorical_crossentropy',
 				  metrics=['acc'])
 	model.summary()
-
-	# train the model
-	callbacks = get_callbacks(args.checkpoint)
 	model.fit(x_train, y_train,
 			  batch_size=args.batch_size,
 			  validation_data=(x_val, y_val),
@@ -46,6 +65,8 @@ def train(args):
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='German Traffic Sign Recognition')
     parser.add_argument('-d', '--base_dir', default='', type=str, help='path to dataset (default: None)')
+    parser.add_argument('-m', '--model', default='LeNet_baseline', type=str, help='Model to use\nChoose between LeNet_baseline, \
+    															LeNet_modified, AlexNet, VGG16 and VGG19 (default: LeNet_baseline)')
     parser.add_argument('-c', '--checkpoint', default='./checkpoint', help='path to checkpoint directory')
     parser.add_argument('-b', '--batch_size', default=1, type=int, help='batch size (default: 1)')
     parser.add_argument('-e', '--epochs', default=20, type=int, help='number of epochs to run (default: 20)')
--- utils/utils.py
+++ utils/utils.py
@@ -19,7 +19,6 @@ def input_preprocess(base_dir, input_shape, num_classes=43):
 			img = cv2.imread(os.path.join(class_path, image))
 			img = Image.fromarray(img, 'RGB')
 			resized_img = img.resize((input_shape[0], input_shape[1]))
-			# resized_img = cv2.resize(img, width, height)
 			x.append(np.array(resized_img))
 			y.append(i)
 	x, y = np.asarray(x), np.asarray(y)
@@ -32,8 +31,8 @@ def one_hot(labels, num_classes=43):
 
 
 # set keras callbacks
-def get_callbacks(ckpt_dir):
-	ckpt_path = os.path.join(ckpt_dir, 'lenet_modified.hdf5')
+def get_callbacks(ckpt_dir, model_name):
+	ckpt_path = os.path.join(ckpt_dir, str.lower(model_name) + '.hdf5')
 	earlystop = EarlyStopping(monitor='val_acc', patience=10, verbose=1, restore_best_weights=False)
 	reducelr = ReduceLROnPlateau(monitor='val_acc', factor=0.5, patience=3, verbose=1, min_lr=1e-6)
 	modelckpt = ModelCheckpoint(ckpt_path, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
