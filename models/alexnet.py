b12f2038fbb86374baa71de6914939e52d51e00b
Anirudh Chakravarthy <anirudh.s.chakravarthy@gmail.com>
2019-07-03 10:27:55 +0530

    Added He initialization

--- models/alexnet.py
+++ models/alexnet.py
@@ -3,18 +3,19 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
 
 
-def AlexNet(num_classes, input_shape=(227,227,3),):
+def AlexNet(num_classes, input_shape=(227,227,3)):
 	'''
 	Standard AlexNet implementation as per the paper
 	NOTE: Input size is (227,227,3), not (224,224,3) as Andrej Karpathy mentioned in his CS231n course.
 	'''
 	model = Sequential()
+	kernel_initializer='he_normal'
 
 	# first block
 	model.add(Conv2D(input_shape=input_shape,
 					 filters=96,
-					 kernel_size=(11,11),
-					 strides=4,
+					 kernel_size=(11,11), strides=4,
+					 kernel_initializer=kernel_initializer,
 					 activation='relu'))
 	model.add(MaxPooling2D(pool_size=(3,3),
 						   strides=2))
@@ -23,8 +24,8 @@ def AlexNet(num_classes, input_shape=(227,227,3),):
 	# second block
 	model.add(Conv2D(filters=256,
 					 kernel_size=(5,5),
-					 strides=1,
-					 padding='same',
+					 strides=1, padding='same',
+					 kernel_initializer=kernel_initializer,
 					 activation='relu'))
 	model.add(MaxPooling2D(pool_size=(3,3),
 						   strides=2))
@@ -32,20 +33,20 @@ def AlexNet(num_classes, input_shape=(227,227,3),):
 
 	model.add(Conv2D(filters=384,
 					 kernel_size=(3,3),
-					 strides=1,
-					 padding='same',
+					 strides=1, padding='same',
+					 kernel_initializer=kernel_initializer,
 					 activation='relu'))
 
 	model.add(Conv2D(filters=384,
 					 kernel_size=(3,3),
-					 strides=1,
-					 padding='same',
+					 strides=1, padding='same',
+					 kernel_initializer=kernel_initializer,
 					 activation='relu'))
 
 	model.add(Conv2D(filters=256,
 					 kernel_size=(3,3),
-					 strides=1,
-					 padding='same',
+					 strides=1, padding='same',
+					 kernel_initializer=kernel_initializer,
 					 activation='relu'))
 
 	model.add(MaxPooling2D(pool_size=(3,3),
--- models/lenet.py
+++ models/lenet.py
@@ -14,27 +14,27 @@ def LeNet_baseline(num_classes, input_shape=(32,32,1)):
 	model.add(Conv2D(input_shape=input_shape,
 					 filters=6, 
 					 kernel_size=(5,5),
-					 activation="tanh"))
+					 activation='tanh'))
 	# Average Pooling, output = (14,14,6)
 	model.add(AveragePooling2D(pool_size=(2,2),
 							   strides=2))
 	# second conv layer, output=(10,10,16)
 	model.add(Conv2D(filters=16, 
 					 kernel_size=(5,5),
-					 activation="tanh"))
+					 activation='tanh'))
 	# Average Pooling, output=(5,5,16)
 	model.add(AveragePooling2D(pool_size=(2,2),
 							   strides=2))
 	# third conv layer, output=(1,1,120)
 	model.add(Conv2D(filters=120, 
 					 kernel_size=(5,5),
-					 activation="tanh"))
+					 activation='tanh'))
 
 	# FC layers
 	model.add(Flatten())
-	model.add(Dense(units=120, activation="tanh"))
-	model.add(Dense(units=84, activation="tanh"))
-	model.add(Dense(units=num_classes, activation="softmax"))
+	model.add(Dense(units=120, activation='tanh'))
+	model.add(Dense(units=84, activation='tanh'))
+	model.add(Dense(units=num_classes, activation='softmax'))
 	return model
 
 
@@ -45,14 +45,17 @@ def LeNet_modified(num_classes, input_shape=(32,32,1)):
 		2. Use Maxpooling instead of average pooling
 		3. Added BatchNormalization after conv layers
 		4. Introduced Dropout after FC layer
+		5. Added He initialization for conv layers
 	'''
 	model = Sequential()
+	kernel_initializer = 'he_normal'
 	
 	# first conv layer, output = (28,28,6)
 	model.add(Conv2D(input_shape=input_shape,
 					 filters=6, 
 					 kernel_size=(5,5),
-					 activation="relu"))
+					 kernel_initializer=kernel_initializer,
+					 activation='relu'))
 	model.add(BatchNormalization())
 	# Average Pooling, output = (14,14,6)
 	model.add(MaxPooling2D(pool_size=(2,2),
@@ -60,7 +63,8 @@ def LeNet_modified(num_classes, input_shape=(32,32,1)):
 	# second conv layer, output=(10,10,16)
 	model.add(Conv2D(filters=16, 
 					 kernel_size=(5,5),
-					 activation="relu"))
+					 kernel_initializer=kernel_initializer,
+					 activation='relu'))
 	model.add(BatchNormalization())
 	# Average Pooling, output=(5,5,16)
 	model.add(MaxPooling2D(pool_size=(2,2),
@@ -68,14 +72,15 @@ def LeNet_modified(num_classes, input_shape=(32,32,1)):
 	# third conv layer, output=(1,1,120)
 	model.add(Conv2D(filters=120, 
 					 kernel_size=(5,5),
-					 activation="relu"))
+					 kernel_initializer=kernel_initializer,
+					 activation='relu'))
 	model.add(BatchNormalization())
 
 	# FC Layers
 	model.add(Flatten())
-	model.add(Dense(units=120, activation="relu"))
+	model.add(Dense(units=120, activation='relu'))
 	model.add(Dropout(0.4))
-	model.add(Dense(units=84, activation="relu"))
+	model.add(Dense(units=84, activation='relu'))
 	model.add(Dropout(0.4))
-	model.add(Dense(units=num_classes, activation="softmax"))
+	model.add(Dense(units=num_classes, activation='softmax'))
 	return model
